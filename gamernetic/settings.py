e8ee265fcd9a680bb945e4761e74a61f9e190ce0
jrdnbrkfld <jrdnbrkfld@icloud.com>
2022-05-29 22:38:55 +0000

    add registration functionality

--- gamernetic/settings.py
+++ gamernetic/settings.py
@@ -32,6 +32,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['gamernetic.herokuapp.com', 'localhost']
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 
 # Application definition
 
@@ -41,6 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
@@ -59,6 +65,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 ROOT_URLCONF = 'gamernetic.urls'
 
