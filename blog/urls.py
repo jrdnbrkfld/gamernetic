81e85e401eeb4391a8ebdb12869c8a6f69d5edf8
jrdnbrkfld <jrdnbrkfld@icloud.com>
2022-07-03 08:39:56 +0000

    add url for add_post page

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
-from . import views
+from .views import AddPostView, PostList, PostDetail, PostLike
 from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
-    
+    path('', PostList.as_view(), name='home'),
+    path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>/', PostLike.as_view(), name='post_like'),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
 ]
