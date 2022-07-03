5f9869f062313260d80bef88f91671d2f23712fc
jrdnbrkfld <jrdnbrkfld@icloud.com>
2022-07-03 09:58:17 +0000

    changed structure of urls previous code would return /summernote/add_post/ in url

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', PostList.as_view(), name='home'),
-    path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>/', PostLike.as_view(), name='post_like'),
-    path('summernote/add_post/', AddPost.as_view(), name='create_post'),
+    path('add_post/', AddPost.as_view(), name='create_post'),
+    path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
 ]
