a04582a204181e3ddebaaf966f3609c1ae405045
Jordan <jrdnbrkfld@icloud.com>
2022-07-06 22:48:08 +0000

    created PostForm class this enables placeholder text to give the user examples of what to write

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import Comment, Post
 from django import forms
 
 
@@ -6,3 +6,21 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class PostForm (forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'excerpt', 'slug', 'featured_image',
+                  'content', 'author', 'status')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder':
+                                            'Give it a catchy title'}),
+            'excerpt': forms.TextInput(attrs={'placeholder':
+                                              'E.g. My first ever post'}),
+            'slug': forms.TextInput(attrs={'placeholder':
+                                           'E.g. this-is-my-post'}),
+            'content': forms.Textarea(attrs={'placeholder':
+                                             'Tell the world what you think!'}),
+        }
