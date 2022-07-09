23844794a23e10114963a2795b6cfe696b794ec7
Jordan <jrdnbrkfld@icloud.com>
2022-07-09 17:57:26 +0000

    link url for update_post

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
-from .views import AddPost, PostList, PostDetail, PostLike
+from .views import AddPost, PostList, PostDetail, PostLike, UpdatePostView
 from django.urls import path
 
 
 urlpatterns = [
     path('', PostList.as_view(), name='home'),
     path('like/<slug:slug>/', PostLike.as_view(), name='post_like'),
-    path('add_post/', AddPost.as_view(), name='create_post'),
+    path('create/', AddPost.as_view(), name='create_post'),
+    path('edit/<slug:slug>/', UpdatePostView.as_view(), name='update_post'),
     path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
 ]
