fbe5a8b11ef64cf151f5fd683834e2cf4fb42f5e
Jordan <jrdnbrkfld@icloud.com>
2022-07-13 22:23:07 +0000

    add authors widget to work in unison with JS code

--- blog/forms.py
+++ blog/forms.py
@@ -23,4 +23,5 @@ class PostForm (forms.ModelForm):
                                            'E.g. this-is-my-post'}),
             'content': forms.Textarea(attrs={'placeholder':
                                              '<p>Wrap your content in these P tags to create a paragraph</p>\n\n<b>Wrap your words in these B tags to make them bold</b>'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'user', 'type': 'hidden'}),
         }
