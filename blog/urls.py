411bc0b0d7f83f00cd192c1e67c536415b349c54
Jordan <jrdnbrkfld@icloud.com>
2022-08-07 17:15:22 +0000

    re ordered code for easier reading

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,10 @@ from .views import AddPost, PostList, PostDetail, PostLike, UpdatePostView, Dele
 
 urlpatterns = [
     path('', PostList.as_view(), name='home'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     path('like/<slug:slug>/', PostLike.as_view(), name='post_like'),
     path('create/', AddPost.as_view(), name='create_post'),
-    path('edit/<slug:slug>/', UpdatePostView.as_view(), name='update_post'),
-    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
+    path('edit/<slug:slug>/', UpdatePostView.as_view(), name='update_post'),
     path('<slug:slug>/delete/', DeletePostView.as_view(), name='delete'),
 ]
