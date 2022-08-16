2489710400f9289503fc68c1fc4b221ed5bb7282
Jordan <jrdnbrkfld@icloud.com>
2022-08-16 16:59:58 +0000

    remove slug field from the form on website

--- blog/forms.py
+++ blog/forms.py
@@ -12,7 +12,7 @@ class CommentForm(forms.ModelForm):
 class PostForm (forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'excerpt', 'slug', 'featured_image',
+        fields = ('title', 'excerpt', 'featured_image',
                   'content', 'author')
 
         widgets = {
@@ -20,8 +20,6 @@ class PostForm (forms.ModelForm):
                                             'Give it a catchy title'}),
             'excerpt': forms.TextInput(attrs={'placeholder':
                                               'E.g. My first ever post'}),
-            'slug': forms.TextInput(attrs={'placeholder':
-                                           'E.g. this-is-my-post'}),
             'content': forms.Textarea(attrs={'placeholder':
                                              '<p>Wrap your content in these P tags to create a paragraph</p>\n\n<b>Wrap your words in these B tags to make them bold</b>'}),
             'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'user', 'type': 'hidden'}),
